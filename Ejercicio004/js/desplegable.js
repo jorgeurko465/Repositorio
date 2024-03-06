45cea97c087911003f941bf7da31fb9322d451de
Jorge <jorgeurjo465@gmail.com>
2024-03-06 14:18:23 +0100

    terminado por hoy

--- Ejercicio004/js/desplegable.js
+++ Ejercicio004/js/desplegable.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function Desplegable() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
